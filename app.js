2a0d277b5115a734b218d816d5aecb6327fb2cfc
SlavenSaric <slavendownload@gmail.com>
2023-10-11 13:18:16 +0200

    handling errors for insupported routes

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require('./models/http-error')
 
 const app = express();
 
 app.use(bodyParser.json())
 
 app.use("/api/places", placesRoutes);
+
+app.use((req,res, next) => {
+    throw new HttpError('Could not find the route.', 404)
+    
+})
 
 app.use((error ,req, res, next) => {
     if(res.headerSent){
