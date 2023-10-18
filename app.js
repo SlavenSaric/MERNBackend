26a868d9c692b4e83878c9ec262e1cd901583dd0
SlavenSaric <slavendownload@gmail.com>
2023-10-18 15:01:19 +0200

    connecting to the databe

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -26,4 +27,12 @@ app.use((error ,req, res, next) => {
     res.json({message: error.message || 'Something went wrong!'})
 });
 
-app.listen(5000);
+mongoose
+.connect('mongodb+srv://slaven:g1zseqlGPm16ELP6@clustermdb.naaddkt.mongodb.net/places?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(5000);
+}).
+catch((err => {
+    console.log(err);
+}));
+
