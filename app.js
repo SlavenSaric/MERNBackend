4dc4e3e631a6e7b10b9decd0bb2972e3647dd6c8
SlavenSaric <slavendownload@gmail.com>
2023-10-31 18:22:56 +0100

    deploy ready

--- app.js
+++ app.js
@@ -42,9 +42,9 @@ app.use((error ,req, res, next) => {
 });
 
 mongoose
-.connect(`mongodb+srv://slaven:g1zseqlGPm16ELP6@clustermdb.naaddkt.mongodb.net/mern?retryWrites=true&w=majority`)
+.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clustermdb.naaddkt.mongodb.net/${process.env.DB_NAME || 'mern-prod'}?retryWrites=true&w=majority`)
 .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
 }).
 catch((err => {
     console.log(err);
