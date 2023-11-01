066c8071cede8c3cee9b3f737e2f5646562e6865
SlavenSaric <slavendownload@gmail.com>
2023-11-01 14:32:57 +0100

    g

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((error ,req, res, next) => {
 mongoose
 .connect('mongodb+srv://slaven:g1zseqlGPm16ELP6@clustermdb.naaddkt.mongodb.net/mern?retryWrites=true&w=majority')
 .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
 }).
 catch((err => {
     console.log(err);
